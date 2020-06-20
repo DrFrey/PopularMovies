5dc959237af82dcf960cc4151208097d78ee9ca7
Anton Vorovatov <avorovatov@gmail.com>
2020-06-20 22:25:49 +0300

    added adapter and viewholder

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,23 +1,45 @@
 package com.example.android.popularmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private TextView mErrorMessage;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRecyclerView = findViewById(R.id.rv_movie_list);
+        mErrorMessage = findViewById(R.id.tv_error_message);
+
+
+
+        mProgressBar = findViewById(R.id.pb_loading);
         new FetchMoviesData().execute();
 
     }
 
+    private void showErrorMessage() {
+
+    }
     public class FetchMoviesData extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
 
         @Override
         protected String doInBackground(Void... voids) {
@@ -26,10 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String s) {
-            TextView mTest = findViewById(R.id.tv_test);
-
             JsonUtilities.jsonExtract(s);
-            mTest.setText("retrieved " + MovieList.getInstance().getMovieList().size() + " movies");
+            mProgressBar.setVisibility(View.INVISIBLE);
         }
     }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/MovieAdapter.java
@@ -0,0 +1,57 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
+
+    private final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/";
+    private final String IMAGE_SIZE = "w185";
+
+    public MovieAdapter() {}
+
+
+    @NonNull
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.movie_list_item, parent, false);
+        return new MovieAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapterViewHolder holder, int position) {
+        Movie movie = MovieList.getInstance().getMovieList().get(position);
+        String coverImageAddress = BASE_IMAGE_URL + IMAGE_SIZE + movie.getPosterPath();
+
+        Picasso.get().load(coverImageAddress).into(holder.mImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(MovieList.getInstance().getMovieList().size() == 0) {
+            return 0;
+        } else {
+            return MovieList.getInstance().getMovieList().size();
+        }
+    }
+
+    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder{
+        public final ImageView mImageView;
+
+        public MovieAdapterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.iv_movie_cover);
+        }
+    }
+
+}
